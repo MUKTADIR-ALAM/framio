4a176d554c83d2d214a8984e693f9f601e914e5d
MUKTADIR-ALAM <muktadiralam38@gmail.com>
2026-04-01 11:51:46 +0600

    add some parts

--- src/app/components/VideoContainer.jsx
+++ src/app/components/VideoContainer.jsx
@@ -8,17 +8,54 @@ const voyage = localFont({
 export default function videoContainer() {
     return (
         <div className='mt-10'>
-            <div className={`${voyage.className} w-11/12 mx-auto text-9xl my-24 font-bold text-center text-[#fc96f4]`}>Weeding flims</div>
-            <iframe
-                className="w-11/12 mx-auto h-[613]"
-                src='https://www.youtube.com/embed/rxib5rh8JjQ?si=JupnWIX17J0wizce'
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-                loading="lazy"
-            ></iframe>
+            <div>
+                <div className="flex flex-col items-center my-8">
+                    <div className={`${voyage.className} w-11/12 mx-auto text-4xl font-bold text-center text-[#fc96f4]`}>Weeding Teaser</div>
+                    <div className={` text-sm font-extralight mt-3`}>Duration- 30 Sec to 1 Min</div>
+                </div>
+                <iframe
+                    className="w-8/12 mx-auto h-[613]"
+                    src='https://www.youtube.com/embed/L12ypEzyZXA?si=AHleu3XsMuNnFsHL'
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                    loading="lazy"
+                ></iframe>
+            </div>
+
+
+            <div className="mt-16">
+
+                <iframe
+                    className="w-11/12 mx-auto h-[613]"
+                    src='https://www.youtube.com/embed/lwFlaIkPGjg?si=Xi_CQxOMs10oBLGG'
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                    loading="lazy"
+                ></iframe>
+
+
+                <div className="flex flex-col items-center my-8">
+                    <div className={`${voyage.className} w-11/12 mx-auto text-4xl font-bold text-center text-[#fc96f4]`}>Weeding Highlight</div>
+                    <div className={` text-sm font-extralight mt-3`}>Duration 3 to 7 Min</div>
+                </div>
+
+                <iframe
+                    className="w-8/12 mx-auto h-[613] mt-12"
+                    src='https://www.youtube.com/embed/H5l4Ips6KHA?si=C2NEGjOKq-NcmfFt'
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                    loading="lazy"
+                ></iframe>
+            </div>
         </div>
     )
 }
